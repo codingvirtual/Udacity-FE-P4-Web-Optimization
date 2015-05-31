d339479f5e9c951005e2fe0aa67a251a471c45ef
Greg Palen <codingvirtual@gmail.com>
2015-05-31 17:19:47 -0500

    add Grunt responsive images code to resize pizzeria image.

---
+++ Gruntfile.js
@@ -0,0 +1,46 @@
+/*
+ After you have changed the settings at "Your code goes here",
+ run this with one of these options:
+  "grunt" alone creates a new, completed images directory
+  "grunt clean" removes the images directory
+  "grunt responsive_images" re-processes images without removing the old ones
+*/
+
+module.exports = function(grunt) {
+
+  grunt.initConfig({
+    responsive_images: {
+      dev: {
+        options: {
+          engine: 'im',
+          sizes: [
+            {
+              width: 360,
+              height: 270,
+              quality: 100,
+              suffix: '_resize'
+            }
+          ]
+        },
+
+        /*
+        You don't need to change this part if you don't change
+        the directory structure.
+        */
+        files: [{
+          expand: true,
+          src: ['*.{gif,jpg,png}'],
+          cwd: 'views/images/',
+          dest: 'views/images'
+        }]
+      }
+    }
+  });
+  
+  grunt.loadNpmTasks('grunt-responsive-images');
+  grunt.loadNpmTasks('grunt-contrib-clean');
+  grunt.loadNpmTasks('grunt-contrib-copy');
+  grunt.loadNpmTasks('grunt-mkdir');
+  grunt.registerTask('default', ['responsive_images']);
+
+};
